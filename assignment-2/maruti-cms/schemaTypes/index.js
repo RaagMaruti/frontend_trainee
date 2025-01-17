994785bae6221eec771940a57c841e305eeb5997
RaagMaruti <raag.joshi@marutitech.com>
2025-01-17 16:12:31 +0530

    feat: changed schema, halfway done

--- assignment-2/maruti-cms/schemaTypes/index.js
+++ assignment-2/maruti-cms/schemaTypes/index.js
@@ -1,6 +1,6 @@
-import home from './home'
+// import home from './home'
 import hero from './hero'
 import clients from './clients'
 import community from './community'
 
-export const schemaTypes = [home, hero, clients, community]
+export const schemaTypes = [hero, clients, community]
