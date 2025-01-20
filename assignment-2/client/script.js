39153fb15f7b779a1a13af61d2f971069f1d788a
RaagMaruti <raag.joshi@marutitech.com>
2025-01-20 17:25:10 +0530

    feat: added script and style for Header component

--- assignment-2/client/script.js
+++ assignment-2/client/script.js
@@ -1,7 +1,71 @@
 const projectId = "7dzx18r8";
 const dataset = "production";
 
-// async function fetchImg(q) {
+async function fetchHeader(q) {
+  const url = `https://${projectId}.apicdn.sanity.io/v2025-01-20/data/query/${dataset}?query=${q}`;
+  const result = await fetch(url)
+    .then((res) => res.json())
+    .then(({ result }) => {
+      return result;
+    })
+    .catch((err) => console.log(err));
+
+  let body = document.querySelector("body");
+  let section = document.createElement("section");
+  body.appendChild(section);
+  section.className = result["name"];
+
+  // logo
+  let logoLink = document.createElement("a");
+  section.appendChild(logoLink);
+  logoLink.href = result["logo"].url;
+  let logoImg = document.createElement("img");
+  logoLink.appendChild(logoImg);
+  logoImg.src = result["logo"].image.asset.url;
+  logoImg.style.height = "1.5em";
+
+  let div = document.createElement("div");
+  section.appendChild(div);
+
+  //links
+  let links = document.createElement("ul");
+  div.appendChild(links);
+  result["links"].forEach((l) => {
+    let li = document.createElement("a");
+    links.appendChild(li);
+    li.innerHTML = l.name;
+    li.href = l.url;
+  });
+
+  // cta
+  let button = document.createElement("button");
+  div.appendChild(button);
+  button.className = "cta";
+  let a = document.createElement("a");
+  button.appendChild(a);
+  a.innerHTML = result["cta"].name;
+  a.href = result["cta"].url;
+}
+
+const queryHeader = `*[_type == "header"][0]{name, cta->{name, url}, links[]->{name, url}, logo->{name, url, image{asset->{url}}}}`;
+fetchHeader(queryHeader);
+
+async function fetchFooter(q) {
+  const url = `https://${projectId}.apicdn.sanity.io/v2025-01-20/data/query/${dataset}?query=${q}`;
+  const result = await fetch(url)
+    .then((res) => res.json())
+    .then(({ result }) => {
+      return result;
+    })
+    .catch((err) => console.log(err));
+
+  console.log(result);
+}
+
+// const queryFooter = `*[_type == "footer"][0]`;
+// fetchFooter(queryFooter);
+
+// async function fetchComponents(q) {
 //   const url = `https://${projectId}.apicdn.sanity.io/v2021-10-21/data/query/${dataset}?query=${q}`;
 //   const result = await fetch(url)
 //     .then((res) => res.json())
@@ -10,54 +74,23 @@ const dataset = "production";
 //     })
 //     .catch((err) => console.log(err));
 
-//   for (let i = 0; i < result.length; i++) {
-//     let poster = `https://cdn.sanity.io/images/${projectId}/${dataset}/`;
-//     imageUrl = result[i].image.asset._ref;
-//     imageUrl = imageUrl.replaceAll("-", ".");
-//     imageUrl = imageUrl.slice(6);
-//     imageUrl = imageUrl.replace(".", "-");
-//     try {
-//       console.log(result[i].text);
-//       document.getElementById(result[i]).src = poster + imageUrl;
-//     } catch (error) {
-//       //   console.log(err);
+//   const components = result["components"];
+
+//   body = document.querySelector("body");
+//   components.forEach((c) => {
+//     section = document.createElement("section");
+//     section.className = c.name;
+//     section.innerHTML = c.name;
+//     body.appendChild(section);
+
+//     switch (c.name) {
+//       case "hero":
+//         break;
 //     }
-//   }
+
+//     console.log(c.name);
+//   });
 // }
 
-// const queryMedia = `*[_type == "media"] | order(text asc)`;
-// fetchImg(queryMedia);
-
-async function fetchComponents(q) {
-  const url = `https://${projectId}.apicdn.sanity.io/v2021-10-21/data/query/${dataset}?query=${q}`;
-  const result = await fetch(url)
-    .then((res) => res.json())
-    .then(({ result }) => {
-      return result;
-    })
-    .catch((err) => console.log(err));
-
-  const components = result.components;
-
-  body = document.querySelector("body");  
-  components.forEach((c) => {
-    section = document.createElement("section");
-    body.appendChild(section);
-    p = document.createElement("p");
-    section.appendChild(p);
-    document.querySelector("p").innerHTML = c.name;
-    console.log(c.name)
-  });
-
-  // for (let i = 0; i < result.length; i++) {
-  //   try {
-  //     console.log(result[i].text);
-  //     document.getElementById(result[i]).innerHTML = result[i].content;
-  //   } catch (err) {
-  //     //   console.log(err);
-  //   }
-  // }
-}
-
-const queryComponents = `*[_type == "home"][0]{components}`;
-fetchComponents(queryComponents);
+// const queryComponents = `*[_type == "home"][0]{components}`;
+// fetchComponents(queryComponents);
