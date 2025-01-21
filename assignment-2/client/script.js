49bca6df49c79c7ca5b4b0c8f968b53e8564e8fa
RaagMaruti <raag.joshi@marutitech.com>
2025-01-21 14:00:23 +0530

    feat: updated client for hero and clients section

--- assignment-2/client/script.js
+++ assignment-2/client/script.js
@@ -3,148 +3,166 @@ const dataset = "production";
 
 async function fetchHeader(q) {
   const url = `https://${projectId}.apicdn.sanity.io/v2025-01-20/data/query/${dataset}?query=${q}`;
-  const result = await fetch(url)
+  await fetch(url)
     .then((res) => res.json())
     .then(({ result }) => {
-      return result;
+      console.log(result);
+
+      let body = document.querySelector("body");
+      let section = document.createElement("section");
+      body.appendChild(section);
+      section.className = result._type;
+
+      // logo
+      let logoLink = document.createElement("a");
+      section.appendChild(logoLink);
+      logoLink.href = result.logo.url;
+      let logoImg = document.createElement("img");
+      logoLink.appendChild(logoImg);
+      logoImg.src = result.logo.image.asset.url;
+
+      let div = document.createElement("div");
+      section.appendChild(div);
+
+      //links
+      let links = document.createElement("ul");
+      div.appendChild(links);
+      result.links.forEach((l) => {
+        let li = document.createElement("a");
+        links.appendChild(li);
+        li.innerHTML = l.name;
+        li.href = l.url;
+      });
+
+      // cta
+      let button = document.createElement("button");
+      div.appendChild(button);
+      button.className = "cta";
+      let a = document.createElement("a");
+      button.appendChild(a);
+      a.innerHTML = result["cta"]["name"];
+      a.href = result["cta"]["url"];
     })
     .catch((err) => console.log(err));
-
-  let body = document.querySelector("body");
-  let section = document.createElement("section");
-  body.appendChild(section);
-  section.className = result["name"];
-
-  // logo
-  let logoLink = document.createElement("a");
-  section.appendChild(logoLink);
-  logoLink.href = result["logo"]["url"];
-  let logoImg = document.createElement("img");
-  logoLink.appendChild(logoImg);
-  logoImg.src = result["logo"]["image"]["asset"]["url"];
-
-  let div = document.createElement("div");
-  section.appendChild(div);
-
-  //links
-  let links = document.createElement("ul");
-  div.appendChild(links);
-  result["links"].forEach((l) => {
-    let li = document.createElement("a");
-    links.appendChild(li);
-    li.innerHTML = l["name"];
-    li.href = l["url"];
-  });
-
-  // cta
-  let button = document.createElement("button");
-  div.appendChild(button);
-  button.className = "cta";
-  let a = document.createElement("a");
-  button.appendChild(a);
-  a.innerHTML = result["cta"]["name"];
-  a.href = result["cta"]["url"];
 }
 
-const queryHeader = `
-  *[_type == "header"][0]{
-  name, 
-  cta->{name, 
-        url}, 
-  links[]->{name, 
-        url}, 
-  logo->{name, 
-        url, 
-        image{asset->{url}}}
-  }
-`;
-fetchHeader(queryHeader);
-
 async function fetchComponents(q) {
-  const url = `https://${projectId}.apicdn.sanity.io/v2021-10-21/data/query/${dataset}?query=${q}`;
-  const result = await fetch(url)
+  const compurl = `https://${projectId}.apicdn.sanity.io/v2021-10-21/data/query/${dataset}?query=${q}`;
+  const result = await fetch(compurl)
     .then((res) => res.json())
     .then(({ result }) => {
       return result;
     })
     .catch((err) => console.log(err));
+  const comps = result.components;
 
-  // console.log(result.components);
-
-  result.components.forEach((comp) => {
-    if (comp._type == "hero") {
-      const getHero = async () => {
-        let q = `*[_type == "home"][0]{
-          components[0]->{
+  for (let i = 0; i < comps.length; i++) {
+    if (comps[i]._type == "hero") {
+      let q = `*[_type == "home"][0]{
+          components[_type == "hero"][0]->{
           tagline, paragraph,
           cta->{name, url},
           image{asset->{url}}}
         }`;
-        const url = `https://${projectId}.apicdn.sanity.io/v2021-10-21/data/query/${dataset}?query=${q}`;
-        const result = await fetch(url)
-          .then((res) => res.json())
-          .then(({ result }) => {
-            return result;
-          })
-          .catch((err) => console.log(err));
+      const url = `https://${projectId}.apicdn.sanity.io/v2021-10-21/data/query/${dataset}?query=${q}`;
+      await fetch(url)
+        .then((res) => res.json())
+        .then(({ result }) => {
+          const hero = result.components;
 
-        const hero = result.components;
-        console.log(hero);
+          let body = document.querySelector("body");
+          let section = document.createElement("section");
+          body.appendChild(section);
+          section.className = comps[i]._type;
 
-        let body = document.querySelector("body");
-        let section = document.createElement("section");
-        body.appendChild(section);
-        section.className = comp._type;
+          // div1
+          let div1 = document.createElement("div");
+          section.appendChild(div1);
 
-        // div1
-        let div1 = document.createElement("div");
-        section.appendChild(div1);
+          let tagline = document.createElement("p");
+          div1.appendChild(tagline);
+          tagline.className = "tagline";
+          tagline.innerHTML = hero.tagline;
 
-        let tagline = document.createElement("p");
-        let paragraph = document.createElement("p");
-        let cta = document.createElement("p");
-        let button = document.createElement("button");
-        let a = document.createElement("a");
+          let paragraph = document.createElement("p");
+          div1.appendChild(paragraph);
+          paragraph.className = "paragraph";
+          paragraph.innerHTML = hero.paragraph;
 
-        div1.appendChild(tagline);
-        div1.appendChild(paragraph);
-        div1.appendChild(cta);
-        div1.appendChild(button);
-        button.appendChild(a);
+          let button = document.createElement("button");
+          div1.appendChild(button);
+          button.className = "cta";
 
-        tagline.className = "tagline";
-        tagline.innerHTML = hero.tagline;
+          let cta = document.createElement("p");
+          div1.appendChild(cta);
 
-        paragraph.className = "paragraph";
-        paragraph.innerHTML = hero.paragraph;
+          let a = document.createElement("a");
+          button.appendChild(a);
+          a.innerHTML = hero.cta.name;
+          a.href = hero.cta.url;
 
-        button.className = "cta";
-        a.innerHTML = hero.cta.name;
-        a.href = hero.cta.url;
+          //div2
+          img = document.createElement("img");
+          section.appendChild(img);
+          img.src = hero.image.asset.url;
+        })
+        .catch((err) => console.log(err));
+    } else if (comps[i]._type == "clients") {
+      let q = `*[_type == "home"][0]{
+          components[_type == "clients"][0]->{
+          paragraph,
+          heading,
+          images[]{asset->{url}}
+          }
+        }`;
+      const url = `https://${projectId}.apicdn.sanity.io/v2021-10-21/data/query/${dataset}?query=${q}`;
+      await fetch(url)
+        .then((res) => res.json())
+        .then(({ result }) => {
+          const clients = result.components;
 
-        //div2
-        let div2 = document.createElement("div");
-        section.appendChild(div2);
-        img = document.createElement("img");
-        div2.appendChild(img);
-        img.src = hero.image.asset.url;
-      };
-      getHero();
+          let body = document.querySelector("body");
+          let section = document.createElement("section");
+          body.appendChild(section);
+          section.className = comps[i]._type;
+
+          //p
+          let heading = document.createElement("p");
+          section.appendChild(heading);
+          heading.innerHTML = clients.heading;
+          heading.className = "heading";
+
+          //p
+          let paragraph = document.createElement("p");
+          section.appendChild(paragraph);
+          paragraph.innerHTML = clients.paragraph;
+          paragraph.className = "paragraph";
+
+          //div for images
+          let div = document.createElement("div");
+          section.appendChild(div);
+
+          clients.images.forEach((c) => {
+            let img = document.createElement("img");
+            div.appendChild(img);
+            img.src = c.asset.url;
+          });
+        })
+        .catch((err) => console.log(err));
     }
-  });
-
-  // body = document.querySelector("body");
-  // components.forEach((c) => {
-  //   section = document.createElement("section");
-  //   section.className = c.name;
-  //   body.appendChild(section);
-
-  //   switch (c.name) {
-  //     case "hero":
-  //       break;
-  //   }
-  // });
+  }
 }
+
+const queryHeader = `
+   *[_type == "header"][0]{
+    _type,
+    cta->{name, url}, 
+    links[]->{name, url}, 
+    logo->{url, image{asset->{url}}
+   }
+ }
+`;
+fetchHeader(queryHeader);
 
 const queryComponents = `*[_type == "home"][0]{components}`;
 fetchComponents(queryComponents);
