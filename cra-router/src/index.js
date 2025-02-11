4033a20a2cf091e9fd14cb409ff676013187d52a
Raag Joshi <raagjoshi67@gmail.com>
2025-02-11 22:58:37 +0530

    removed web vitals

--- cra-router/src/index.js
+++ cra-router/src/index.js
@@ -10,7 +10,6 @@ import Lifecycle from "./pages/Lifecycle";
 import Header from "./components/Header";
 
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -26,21 +25,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-// Rules of React
-// JSX - 1) single root element, 2) close tags, 3) attributes are camelCase, 4) {} to access JS
-// Keys in lists - UUID
-// Components should not change variables outside their scope, even props
-// Pure functions if not changed, does not re-render
-// React has own UI Tree, smaller tree, smaller bundler
-// Only state variable changes can cause re-rendering, each component has independent state
-// Render (painting) - 1) trigger initial render, 2) render changed components, 3) commit to DOM
-// State changes are only reflected on render, in between no values are changed
-// Updater function - to change state multiple time between render - n => n + 1
-// Do not change object or array elements, set new objects or arrays, do not use mutating methods
-// Link - normal routing, useNavigate - forced routing on event
