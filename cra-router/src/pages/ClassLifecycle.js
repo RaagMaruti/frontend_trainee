103c25b5ab74171c77224ca2b230011e8bbaa7a7
RaagMaruti <raag.joshi@marutitech.com>
2025-02-14 16:08:26 +0530

    feat: added examples of hooks and custom hooks

--- cra-router/src/pages/ClassLifecycle.js
+++ cra-router/src/pages/ClassLifecycle.js
@@ -1,95 +1,90 @@
 import React, { Component } from "react";
 
 class ClassLifecycle extends Component {
-	count = {
-		constructor: 0,
-		render: 0,
-		derived: 0,
-	};
+  count = 0;
 
-	constructor(props) {
-		console.log("Constructor lifecycle", props);
-		super(props);
-		this.count.constructor++;
-		this.state = {
-			count: 0,
-			name: "joshi",
-		};
-	}
+  constructor(props) {
+    console.log("Constructor lifecycle - 1", props);
+    super(props);
+    this.count++;
+    this.state = {
+      count: 0,
+      name: "joshi",
+    };
+  }
 
-	static getDerivedStateFromProps(props, state) {
-		console.log("getDerivedStateFromProps: Sync state with props", state);
-		return null;
-		// return { name: props.name };
-		// returning updated state
-		// return null if not updating state
-	}
+  static getDerivedStateFromProps(props, state) {
+    console.log(
+      "getDerivedStateFromProps: Sync state with props - 2, 5",
+      state,
+      props
+    );
+    return null;
+    // return { name: props.name };
+    // returning updated state
+    // return null if not updating state
+  }
 
-	componentDidMount() {
-		console.log("componentDidMount: Yes it has been rendered");
-	}
+  componentDidMount() {
+    console.log("componentDidMount: Yes it has been rendered - 4");
+  }
 
-	shouldComponentUpdate(props, state) {
-		console.log("shouldComponentUpdate", state);
-		if (state.count < 4) {
-			return true;
-		}
-		return false;
-		// only rendering stops, states still gets updated
-	}
+  shouldComponentUpdate(props, state) {
+    console.log("shouldComponentUpdate - 6", state);
+    if (state.count < 4) {
+      return true;
+    }
+    return false;
+    // only rendering stops, states still gets updated
+  }
 
-	getSnapshotBeforeUpdate(props, state) {
-		console.log(
-			"getSnapshotBeforeUpdate: Snapshot before update",
-			props,
-			state
-		);
-		return null;
-	}
+  getSnapshotBeforeUpdate(props, state) {
+    console.log(
+      "getSnapshotBeforeUpdate: Snapshot before update - 8",
+      props,
+      state
+    );
+    return null;
+  }
 
-	componentDidUpdate(props, state, snapshot) {
-		console.log(
-			"componentDidUpdate: Component updated",
-			props,
-			state,
-			snapshot
-		);
-	}
+  componentDidUpdate(props, state) {
+    console.log("componentDidUpdate: Component updated - 9", props, state);
+  }
 
-	componentWillUnmount() {
-		console.log("componentWillUnmount: Component is about to be removed");
-	}
+  componentWillUnmount() {
+    console.log("componentWillUnmount: Component is about to be removed - 10");
+  }
 
-	// componentDidCatch(error, info) {
-	// 	console.log("componentDidCatch: Caught an error", error, info);
-	// }
+  // componentDidCatch(error, info) {
+  // 	console.log("componentDidCatch: Caught an error", error, info);
+  // }
 
-	handleIncrement = () => {
-		this.setState((state) => ({ count: state.count + 1 }));
-	};
+  handleIncrement = () => {
+    this.setState((state) => ({ count: state.count + 1 }));
+  };
 
-	render() {
-		console.log("Rendering component");
-		return (
-			<div style={{ padding: "1em" }}>
-				<h2>React Lifecycle Methods</h2>
-				<p>Name: {this.state.name}</p>
-				<p>Count: {this.state.count}</p>
-				<button onClick={this.handleIncrement}>Increment</button>
-			</div>
-		);
-	}
+  render() {
+    console.log("Rendering component - 3, 7");
+    return (
+      <div style={{ padding: "1em" }}>
+        <h2>React Lifecycle Methods</h2>
+        <p>Name: {this.state.name}</p>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.handleIncrement}>Increment</button>
+      </div>
+    );
+  }
 }
 
 export default class Caller extends ClassLifecycle {
-	constructor(props) {
-		console.log("Constructor Caller");
-		super(props);
-	}
+  constructor(props) {
+    console.log("Constructor Caller - 1");
+    super(props);
+  }
 
-	render() {
-		return <ClassLifecycle name="raag" />;
-	}
+  render() {
+    return <ClassLifecycle name="raag" />;
+  }
 }
 
 // mounting
