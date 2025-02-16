85ac1be39bb301e14a58fe207e32793c78e39dbf
Raag Joshi <raagjoshi67@gmail.com>
2025-02-17 00:54:01 +0530

    feat: form hook

--- cra-router/src/pages/Custom.js
+++ cra-router/src/pages/Custom.js
@@ -1,38 +1,230 @@
 import { useState, useEffect } from "react";
 
-function useFetch(url) {
-  const [data, setData] = useState(null);
-  const [mobile, setMobile] = useState(false);
+// function useFetch(url) {
+// 	const [data, setData] = useState(null);
+// 	const [mobile, setMobile] = useState(false);
 
-  useEffect(() => {
-    console.log(window.innerWidth);
-    if (window.innerWidth < 576) {
-      setMobile(true);
-    } else {
-      setMobile(false);
-    }
-  }, []);
+// 	useEffect(() => {
+// 		console.log(window.innerWidth);
+// 		if (window.innerWidth < 576) {
+// 			setMobile(true);
+// 		} else {
+// 			setMobile(false);
+// 		}
+// 	}, []);
 
-  useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-      });
-  }, [url]);
+// 	useEffect(() => {
+// 		fetch(url)
+// 			.then((res) => res.json())
+// 			.then((data) => {
+// 				setData(data);
+// 			});
+// 	}, [url]);
 
-  return { data, mobile };
+// 	return { data, mobile };
+// }
+
+// export default function Custom() {
+// 	const { data, mobile } = useFetch(
+// 		"https://jsonplaceholder.typicode.com/users"
+// 	);
+
+// 	return (
+// 		<div>
+// 			<ul>{data && data.map((user) => <li key={user.id}>{user.name}</li>)}</ul>
+// 			<p>Fetched on a {mobile ? "mobile" : "desktop"}.</p>
+// 		</div>
+// 	);
+// }
+
+function useFormHandler(initialValues) {
+	const [values, setValues] = useState(initialValues);
+	const [errors, setErrors] = useState({});
+
+	const validateField = (name, value) => {
+		let error = "";
+
+		if (!value) {
+			error = "required field";
+		} else if (name === "email" && !/^\S+@\S+\.\S+$/.test(value)) {
+			error = "invalid email address";
+		} else if (name === "phone" && !/^[0-9]{8,15}$/.test(value)) {
+			error = "invalid phone number";
+		}
+
+    setErrors((prev) => ({ ...prev, [name]: error }));
+	};
+
+	const handleChange = (e) => {
+		const { name, value, type, checked } = e.target;
+		let newValue = type === "checkbox" ? checked : value;
+
+		if (name === "firstName" || name === "lastName") {
+			newValue = value.replace(/^[^a-zA-Z]$/g, "");
+		} else if (name === "phone") {
+			newValue = value.replace(/^[^0-9]$/g, "");
+		}
+
+		setValues((prev) => ({ ...prev, [name]: newValue }));
+		validateField(name, newValue);
+	};
+
+	const validateForm = () => {
+		const newErrors = {};
+
+		Object.entries(values).forEach(([key, value]) => {
+			validateField(key, value);
+			if (!value) {
+				newErrors[key] = "required field";
+			}
+		});
+
+		setErrors(newErrors);
+		return Object.keys(newErrors).length === 0;
+	};
+
+	const handleSubmit = (callback) => (e) => {
+		e.preventDefault();
+		if (validateForm()) {
+			callback(values);
+		}
+	};
+
+	return { values, errors, handleChange, handleSubmit };
 }
 
 export default function Custom() {
-  const { data, mobile } = useFetch(
-    "https://jsonplaceholder.typicode.com/users"
-  );
+	const { values, errors, handleChange, handleSubmit } = useFormHandler({
+		firstName: "",
+		lastName: "",
+		email: "",
+		phone: "",
+		hear: "",
+		company: "",
+		help: "",
+		consent: false,
+	});
 
-  return (
-    <div>
-      <ul>{data && data.map((user) => <li key={user.id}>{user.name}</li>)}</ul>
-      <p>Fetched on a {mobile ? "mobile" : "desktop"}.</p>
-    </div>
-  );
+	const onSubmit = (data) => {
+		console.log("Form Submitted Successfully!", data);
+		alert("Form Submitted Successfully!");
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div>
+					<label>first name</label>
+					<input
+						maxLength="50"
+						type="text"
+						name="firstName"
+						value={values.name}
+						onChange={handleChange}
+						onBlur={handleChange}
+					/>
+					{errors.name && <span style={{ color: "red" }}>{errors.name}</span>}
+				</div>
+
+				<div>
+					<label>last name</label>
+					<input
+						maxLength="50"
+						type="text"
+						name="lastName"
+						value={values.name}
+						onChange={handleChange}
+						onBlur={handleChange}
+					/>
+					{errors.name && <span style={{ color: "red" }}>{errors.name}</span>}
+				</div>
+
+				<div>
+					<label>email</label>
+					<input
+						maxLength="50"
+						type="email"
+						name="email"
+						value={values.email}
+						onChange={handleChange}
+						onBlur={handleChange}
+					/>
+					{errors.email && <span style={{ color: "red" }}>{errors.email}</span>}
+				</div>
+
+				<div>
+					<label>phone number</label>
+					<input
+						type="text"
+						name="phone"
+						value={values.phone}
+						onChange={handleChange}
+						onBlur={handleChange}
+					/>
+					{errors.phone && <span style={{ color: "red" }}>{errors.phone}</span>}
+				</div>
+
+				<div>
+					<label>how did you hear about us?</label>
+					<input
+						maxLength="50"
+						type="text"
+						name="hear"
+						value={values.hear}
+						onChange={handleChange}
+						onBlur={handleChange}
+					/>
+					{errors.hear && <span style={{ color: "red" }}>{errors.hear}</span>}
+				</div>
+
+				<div>
+					<label>company</label>
+					<input
+						maxLength="50"
+						type="text"
+						name="company"
+						value={values.company}
+						onChange={handleChange}
+						onBlur={handleChange}
+					/>
+					{errors.company && (
+						<span style={{ color: "red" }}>{errors.company}</span>
+					)}
+				</div>
+
+				<div>
+					<label>what can we help you with?</label>
+					<br />
+					<textarea
+						maxLength="500"
+						rows="5"
+						cols="50"
+						name="help"
+						value={values.help}
+						onChange={handleChange}
+						onBlur={handleChange}
+					/>
+					{errors.help && <span style={{ color: "red" }}>{errors.help}</span>}
+				</div>
+
+				<div>
+					<label>
+						<input
+							type="checkbox"
+							name="consent"
+							checked={values.consent}
+							onChange={handleChange}
+							onBlur={handleChange}
+						/>
+						I consent to the terms and conditions
+					</label>
+					{errors.consent && (
+						<span style={{ color: "red" }}>{errors.consent}</span>
+					)}
+				</div>
+
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
 }
--- cra-router/src/pages/Form.js
+++ cra-router/src/pages/Form.js
@@ -1,50 +1,136 @@
 import { useForm } from "react-hook-form";
+import "./Form.css";
 
-export default function Form() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+export default function App() {
+	const {
+		register,
+		formState: { errors },
+		setValue,
+		setError,
+		clearErrors,
+		handleSubmit,
+	} = useForm();
 
-  const onSubmit = (data) => {
-    console.log("Form Data:", data);
-  };
+	const onSubmit = (data) => console.log(data);
 
-  return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div>
-        <label>Name:</label>
-        <input {...register("name", { required: "Name is required" })} />
-        {errors.name && <p style={{ color: "red" }}>{errors.name.message}</p>}
-      </div>
+	const handleChange = (e) => {
+		let { name, value } = e.target;
 
-      <div>
-        <label>Email:</label>
-        <input
-          {...register("email", {
-            required: "Email is required",
-            pattern: { value: /^\S+@\S+$/i, message: "Invalid email" },
-          })}
-        />
-        {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
-      </div>
+		if (value === "") {
+			setError(name, { type: "required", message: "required field" });
+		}  else{
+			clearErrors(name);
+			if (name === "firstName" || name === "lastName") {
+				value = value.replace(/^[^a-zA-Z]$/g, "");
+			} else if (name === "email") {
+				const pattern = /^\S+@\S+\.\S+$/;
+				if (!pattern.test(value)) {
+					setError("email", {
+						type: "invalid",
+						message: "invalid email address",
+					});
+				}
+			} else if (name === "phone") {
+				value = value.replace(/^[^0-9]$/g, "");
+				const pattern = /^[0-9]{8,15}$/;
+				if (!pattern.test(value)) {
+					setError("phone", {
+						type: "invalid",
+						message: "invalid phone number",
+					});
+				}
+			}
+			setValue(name, value);
+		}
+	};
 
-      <div>
-        <label>Age:</label>
-        <input
-          type="number"
-          {...register("age", {
-            required: "Age is required",
-            min: { value: 18, message: "Must be at least 18" },
-          })}
-        />
-        {errors.age && <p style={{ color: "red" }}>{errors.age.message}</p>}
-      </div>
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<label>First Name *</label>
+			<input
+				{...register("firstName", {
+					required: "required field",
+					maxLength: 50,
+				})}
+				onChange={handleChange}
+				onBlur={handleChange}
+				aria-invalid={errors.firstName ? "true" : "false"}
+			/>
+			<span role="alert">{errors.firstName?.message}</span>
+			<br />
 
-      <button type="submit">Submit</button>
-    </form>
-  );
+			<label>Last Name *</label>
+			<input
+				{...register("lastName", {
+					required: { value: true, message: "required field" },
+					maxLength: 50,
+				})}
+				onChange={handleChange}
+				onBlur={handleChange}
+				aria-invalid={errors.lastName ? "true" : "false"}
+			/>
+			<span role="alert">{errors.lastName?.message}</span>
+			<br />
+
+			<label>Email Address *</label>
+			<input
+				{...register("email", {
+					required: { value: true, message: "required field" },
+					maxLength: 50,
+					pattern: {
+						value: /^\S+@\S+\.\S+$/,
+						message: "invalid email address",
+					},
+				})}
+				aria-invalid={errors.email ? "true" : "false"}
+				onChange={handleChange}
+				onBlur={handleChange}
+			/>
+			<span role="alert">{errors.email?.message}</span>
+			<br />
+
+			<label>Phone Number *</label>
+			<input
+				{...register("phone", {
+					required: { value: true, message: "required field" },
+					pattern: {
+						value: /^[0-9]{8,15}$/,
+						message: "invalid phone number",
+					},
+				})}
+				aria-invalid={errors.phone ? "true" : "false"}
+				onChange={handleChange}
+				onBlur={handleChange}
+			/>
+			<span role="alert">{errors.phone?.message}</span>
+			<br />
+
+			<label>How Did You Hear About Us?</label>
+			<input {...register("hear", { maxLength: 100 })} />
+			<br />
+
+			<label>Company</label>
+			<input {...register("company", { maxLength: 50 })} />
+			<br />
+
+			<label>What Can We Help You With?</label>
+			<br />
+			<textarea rows="5" cols="50" {...register("help", { maxLength: 500 })} />
+			<br />
+
+			<input
+				type="checkbox"
+				{...register("consent", { required: "please tick the checkbox" })}
+				aria-invalid={errors.consent ? "true" : "false"}
+			/>
+			<label>
+				I consent to processing of my personal data entered above for Maruti
+				Techlabs to contact me.
+			</label>
+			<span role="alert">{errors.consent?.message}</span>
+			<br />
+
+			<input type="submit" />
+		</form>
+	);
 }
