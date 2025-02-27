047638ac74de23e65a5bcd040ee5332813469975
RaagMaruti <raag.joshi@marutitech.com>
2025-02-27 19:35:55 +0530

    feat: portals examples

--- cra-router/src/pages/Data.js
+++ cra-router/src/pages/Data.js
@@ -1,43 +1,58 @@
 import { useState } from "react";
-import { Outlet, useNavigate, useLocation, useSearchParams } from "react-router-dom";
+import { createPortal } from "react-dom";
+
+import {
+  Outlet,
+  useNavigate,
+  useLocation,
+  useSearchParams,
+} from "react-router-dom";
+
+function Modal({ lines }) {
+  const [open, setOpen] = useState(true);
+
+  if (!open) return <></>;
+
+  return (
+    <div className="modal-overlay" onClick={() => setOpen(false)}>
+      <div className="modal-content">
+        <ul>
+          {lines.map((i) => (
+            <li key={i}>{i}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default function Data() {
-  const [data, setData] = useState(false);
+  const [show, setShow] = useState(false);
   const navigate = useNavigate();
   let location = useLocation();
   console.log(location);
   const p = useSearchParams();
   console.log(p[0].get("team"));
 
-  function handleClick(e) {
-		e.stopPropagation();
-		setData(!data);
-		data ? alert("Hiding Data") : alert("Showing Data");
-		data && navigate("bio/raag/fullstack");
-	}
-  
+  function handleClick() {
+    setShow(true);
+    show && navigate("bio/raag/fullstack");
+  }
+
   const lines = [
-		"first line",
-		"second line",
-		"third line",
-		"fourth line",
-		"fifth line",
-	];
+    "first line",
+    "second line",
+    "third line",
+    "fourth line",
+    "fifth line",
+  ];
 
   return (
-		<div style={{ padding: "1em" }} onClick={handleClick}>
-			<h1>This is the Data Page</h1>
-			<button onClick={handleClick}>
-				Click button or div to change show/hide data
-			</button>
-			{data && (
-				<ul>
-					{lines.map((i) => (
-						<li key={i}>{i}</li>
-					))}
-				</ul>
-			)}
-			<Outlet />
-		</div>
-	);
+    <div style={{ padding: "1em" }}>
+      <h1>This is the Data Page</h1>
+      <button onClick={handleClick}>show data</button>
+      {show && createPortal(<Modal lines={lines} />, document.body)}
+      <Outlet />
+    </div>
+  );
 }
