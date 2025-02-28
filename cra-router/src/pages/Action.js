e9b699d1995ea5d2f9f6ff169e94642b4854f12e
RaagMaruti <raag.joshi@marutitech.com>
2025-02-28 18:15:13 +0530

    fix: minor changes

--- cra-router/src/pages/Action.js
+++ cra-router/src/pages/Action.js
@@ -2,22 +2,21 @@ import React, { useActionState } from "react";
 
 async function incrementCounter(prevCount) {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  return prevCount + 1; // Returns new count
+  return prevCount + 1;
 }
 
 export default function Counter() {
   const [count, dispatch, isPending] = useActionState(incrementCounter, 0);
 
   return (
-		<div>
-			<h2>Counter: {count}</h2>
-			<button onClick={() => dispatch()} disabled={isPending}>
-				{isPending ? "Updating..." : "Increment"}
-			</button>
-		</div>
-	);
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={() => dispatch()} disabled={isPending}>
+        {isPending ? "Updating..." : "Increment"}
+      </button>
+    </div>
+  );
 }
-
 
 // we can do this all with just useState, useEffect.
 // it is mostly used on "use server" components
--- cra-router/src/pages/Portal.js
+++ cra-router/src/pages/Portal.js
@@ -3,48 +3,40 @@ import { createPortal } from "react-dom";
 
 const Tooltip = () => {
   return createPortal(
-		<div
-			style={{
-				position: "absolute",
-				top: 100,
-				left: 100,
-				background: "black",
-				color: "white",
-				padding: "5px 10px",
-				marginRight: "100px",
-				borderRadius: "5px",
-				whiteSpace: "nowrap",
-				zIndex: 1000,
-			}}
-		>
-			<>"overflow"</>
-		</div>,
-		document.body
-	);
+    <div
+      style={{
+        position: "absolute",
+        top: 100,
+        left: 100,
+        fontSize: 30,
+        zIndex: 1000,
+      }}
+    >
+      <>"overflow"</>
+    </div>,
+    document.body
+  );
 };
 
 const App = () => {
   const [msg, setMsg] = useState(false);
 
   return (
-		<div
-			style={{
-				width: "200px",
-				height: "100px",
-				overflow: "hidden",
-				border: "2px solid black",
-			}}
-		>
-			<button
-				onClick={() => setMsg(!msg)}
-				style={{ margin: "40px" }}
-			>
-				click me
-			</button>
-			{msg && <Tooltip text="overflow" />}
-			{msg && <>over</>}
-		</div>
-	);
+    <div
+      style={{
+        width: "200px",
+        height: "100px",
+        overflow: "hidden",
+        border: "2px solid black",
+      }}
+    >
+      <button onClick={() => setMsg(!msg)} style={{ margin: "40px" }}>
+        click me
+      </button>
+      {msg && <>overflow</>}
+      {msg && <Tooltip text="overflow" />}
+    </div>
+  );
 };
 
 export default App;
