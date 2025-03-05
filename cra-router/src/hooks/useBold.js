47a24004480717fadad5959191c0b2b4d5273088
RaagMaruti <raag.joshi@marutitech.com>
2025-03-05 13:02:25 +0530

    fix: removing "cra-router"

--- cra-router/src/hooks/useBold.js
+++
@@ -1,8 +0,0 @@
-import { useDebugValue, useState } from "react";
-
-export default function useBold(jsx) {
-  const [ui, setUi] = useState(jsx);
-  useDebugValue(jsx ? "ui is there" : "no ui");
-
-  return <b>{ui}</b>;
-}
