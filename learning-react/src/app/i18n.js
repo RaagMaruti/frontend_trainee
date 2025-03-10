f1f5af746b6721b77acd417d34dc02f7f6c037af
RaagMaruti <raag.joshi@marutitech.com>
2025-03-10 18:31:46 +0530

    feat: examples

--- learning-react/src/app/i18n.js
+++ learning-react/src/app/i18n.js
@@ -1,22 +1,28 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-
 // for big websites store in separate JSON
 await i18n.use(initReactI18next).init({
-	lng: "gu",
-	resources: {
-		en: {
-			translation: {
-				hello: "Hello, how are you?",
-			},
-		},
-		gu: {
-			translation: {
-				hello: "નમસ્તે, કેમ છો?",
-			},
-		},
-	},
+  lng: "en",
+  resources: {
+    en: {
+      translation: {
+        hello: "{{name}} - Hello, how are you?",
+        nested: {
+          key: "actual nested key",
+        },
+        error: {
+          unspecific: "Something went wrong.",
+          404: "The page was not found.",
+        },
+      },
+    },
+    gu: {
+      translation: {
+        namste: "{{name}} - નમસ્તે, કેમ છો?",
+      },
+    },
+  },
 });
 
 export default i18n;
--- learning-react/src/components/Profile.js
+++ learning-react/src/components/Profile.js
@@ -1,19 +1,30 @@
-"use client"
+"use client";
 
 import { useContext } from "react";
 import { ProfileContext } from "../app/page.js";
 
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 export default function Profile() {
   const { t, i18n } = useTranslation();
   // context variables are global but read-only
   const username = useContext(ProfileContext);
   return (
-		<div>
-			<p>
-				<u>{username}</u>: {t("hello", "default if nothing loaded")}
-			</p>
-		</div>
-	);
+    <div>
+      <p>
+        {t(
+          "namste",
+          { name: username },
+          { defaultValue: "lang not available" }
+        )}{" "}
+        <br />
+        {t(
+          "hello",
+          { name: username },
+          { defaultValue: "lang not available" }
+        )}{" "}
+        <br />
+      </p>
+    </div>
+  );
 }
