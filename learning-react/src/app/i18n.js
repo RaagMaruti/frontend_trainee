12f3ccb0f2c4d70b27397fa4d682447e7b81f53e
Raag Joshi <raagjoshi67@gmail.com>
2025-03-11 00:58:13 +0530

    feat: i18next examples

--- learning-react/src/app/i18n.js
+++ learning-react/src/app/i18n.js
@@ -3,27 +3,36 @@ import { initReactI18next } from "react-i18next";
 
 // for big websites store in separate JSON
 await i18n.use(initReactI18next).init({
-  lng: "en",
-  resources: {
-    en: {
-      translation: {
-        hello: "{{name}} - Hello, how are you?",
-        nested: {
-          key: "actual nested key",
-        },
-        error: {
-          unspecific: "Something went wrong.",
-          404: "The page was not found.",
-        },
-        cash: "{{value}} USD",
-      },
-    },
-    gu: {
-      translation: {
-        namste: "{{name}} - નમસ્તે, કેમ છો?",
-      },
-    },
-  },
+	lng: "en",
+	resources: {
+		en: {
+			translation: {
+				hello: "{{name}} - Hello, how are you?",
+				nested: {
+					key: "actual nested key",
+				},
+				error: {
+					unspecific: "Something went wrong.",
+					404: "The page was not found.",
+				},
+				cash: "{{value, number(minimumFractionDigits: 2)}} USD",
+				count_one: "{{count}} item",
+				count: "{{count}} items",
+
+				msg1: "1",
+				msg2: "2",
+				msgs: "$(msg1), $(msg2)",
+			},
+		},
+		gu: {
+			translation: {
+				namste: "{{name}} - નમસ્તે, કેમ છો?",
+				cash: "INR {{value, number(minimumFractionDigits: 2)}}",
+				count_one: "{{count}} item",
+				count: "{{count}} items",
+			},
+		},
+	},
 });
 
 export default i18n;
--- learning-react/src/components/Profile.js
+++ learning-react/src/components/Profile.js
@@ -7,24 +7,39 @@ import { useTranslation } from "react-i18next";
 
 export default function Profile() {
   const { t, i18n } = useTranslation();
+
+  function handleClick(){
+    const newLang = i18n.language === "en" ? "gu" : "en";
+    i18n.changeLanguage(newLang);
+  }
+
   // context variables are global but read-only
   const username = useContext(ProfileContext);
   return (
-    <div>
-      <p>
-        {t(
-          "namste",
-          { name: username },
-          { defaultValue: "lang not available" }
-        )}{" "}
-        <br />
-        {t(
-          "hello",
-          { name: username },
-          { defaultValue: "lang not available" }
-        )}{" "}
-        <br />
-      </p>
-    </div>
-  );
+		<div style={{ margin: "1em" }}>
+			{t("namste", { name: username }, { defaultValue: "lang not available" })}
+			<br />
+			{t("hello", { name: username }, { defaultValue: "lang not available" })}
+			<br />
+			{t("cash", {
+				value: 20000.2,
+				minimumFractionDigits: 3,
+				currency: "INR",
+			})}
+			<br />
+			{t("count", { count: 1 })}
+			<br />
+			{t("count", { count: 100 })}
+			<br />
+
+			{t("msgs")}
+			<br />
+			<button
+				style={{ border: "1px solid black", padding: "2px" }}
+				onClick={handleClick}
+			>
+				change language
+			</button>
+		</div>
+	);
 }
